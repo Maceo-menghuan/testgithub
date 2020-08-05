da3e033f7cc6cd0e7e4d9dd3139207cd84fab46e
e <wh_yinhanwei@yunlizhihui.com>
2020-08-05 20:15:51 +0800

    测试冲突01机提交

--- src/main/java/com/test/child/Child.java
+++ src/main/java/com/test/child/Child.java
@@ -1,21 +1,37 @@
 package com.test.child;
 
+/**
+ * @version: 2.0
+ * @author wh_yi
+ * @date: 2020/08/05 20:13
+ */
+
 public class Child {
     private String name;
     private Integer age;
     private String sex;
     private String birth;
     private String high;
+    private String weight;
 
     @Override
     public String toString() {
-        return "TestGitHubApplication{" +
+        return "Child{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", sex='" + sex + '\'' +
                 ", birth='" + birth + '\'' +
                 ", high='" + high + '\'' +
+                ", weight='" + weight + '\'' +
                 '}';
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
     }
 
     public String getHigh() {
