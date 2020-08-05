602cb47c219319e5fa2996d4a20eeb082c0b9a3f
e <wh_yinhanwei@yunlizhihui.com>
2020-08-05 16:07:49 +0800

    test github connect idea

--- src/main/java/com/test/TestGitHubApplication.java
+++ src/main/java/com/test/TestGitHubApplication.java
@@ -5,11 +5,13 @@ package com.test;
  *@author: y
  *@date: 2020/08/05 15:07
  */
+
 public class TestGitHubApplication {
     private String name;
     private Integer age;
     private String sex;
     private String birth;
+    private String high;
 
     @Override
     public String toString() {
@@ -17,10 +19,18 @@ public class TestGitHubApplication {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", sex='" + sex + '\'' +
-                ", birth=" + birth +
+                ", birth='" + birth + '\'' +
+                ", high='" + high + '\'' +
                 '}';
     }
 
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
     public String getBirth() {
         return birth;
     }
@@ -59,12 +69,14 @@ public class TestGitHubApplication {
         test.setAge(14);
         test.setSex("man");
         test.setBirth("1999/08/15");
+        test.setHigh("185cm");
         String print = test.toString();
         System.out.println(print);
         System.out.println("name=" + test.name);
         System.out.println("age=" + test.age);
         System.out.println("sex=" + test.sex);
         System.out.println("birt=" + test.birth);
+        System.out.println("high=" + test.high);
     }
 
 }
